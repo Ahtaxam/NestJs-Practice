7e06eb4f25d874b00ecd66c1a58d2dc366b6b06e
Hamza Yaseen <hamzza.yaseen@cooperativecomputing.com>
2023-09-14 12:29:55 +0500

    feat: create job dto

--- src/jobs/dto/createjob.dto.ts
+++ src/jobs/dto/createjob.dto.ts
@@ -1,9 +1,56 @@
+import { Type } from "class-transformer";
+import { ArrayMinSize, IsArray, IsEmail, IsIn, IsInt, IsNotEmpty, IsObject, IsOptional, IsString, ValidateNested } from "class-validator";
+
+enum JobType {
+    FULL_TIME,
+    PART_TIME
+}
+
+export class LocationDto {
+    @IsString()
+    @IsNotEmpty()
+    city:string;
+
+    @IsString()
+    @IsNotEmpty()
+    country:string
+}
+
 export class CreateJobDto{
+    @IsString()
+    @IsNotEmpty()
     name:string;
+
+    @IsString()
+    @IsNotEmpty()
     title:string;
+
+    @IsEmail()
+    @IsNotEmpty()
     email:string;
-    type:string;
+
+    @IsIn(Object.keys(JobType))
+    @IsOptional()
+    type?:string;
+
+    @IsString({each:true})
+    @ArrayMinSize(1)
     tags:string[];
-    salary:number
+
+    @IsInt()
+    salary:number;
+
+    @ValidateNested()
+    @IsObject()
+    @Type(() => LocationDto)
+    location:LocationDto
+
+
+
+// validate array of object
+    // @ValidateNested({each:true})
+    // @IsArray()
+    // @Type(() => LocationDto)
+    // location:LocationDto[]
 
 }
